09ced79ac8d3c722b88d9aec6a2263a629567d2a
JeremyRollo <41007720@parisnanterre.fr>
2025-04-08 13:05:09 +0200

    Correction fautes orthographe
    Correction fautes orthographe

--- frontend/src/components/pages/DashboardPage.js
+++ frontend/src/components/pages/DashboardPage.js
@@ -14,33 +14,33 @@ const DashboardPage = () => {
             Tableau de Bord des Données
           </Typography>
           <Typography variant="body1">
-            Explorez ici des statistiques détaillées notre jeux de données sur les caractéristiques des voitures. Grâce aux graphiques interactifs ci-dessous, vous pourrez approfondir votre compréhension des tendances et des facteurs clés liés à ces véhicules.
+            Explorez ici des statistiques détaillées de notre jeu de données sur les caractéristiques des voitures. Grâce aux graphiques interactifs ci-dessous, vous pourrez approfondir votre compréhension des tendances et des facteurs clés liés à ces véhicules.
           </Typography>
 
           <Box mt={6}>
             <Typography variant="h4" gutterBottom>
-              Commment l'ancienneté d'une voiture affecte-t-elle son prix ?
+              Comment l'ancienneté d'une voiture affecte-t-elle son prix ?
             </Typography>
             <Graph1 />
           </Box>
 
           <Box mt={6}>
             <Typography variant="h4" gutterBottom>
-              Quel type de transmission est le plus économiques ?
+              Quel type de transmission est le plus économique ?
             </Typography>
             <Graph2 />
           </Box>
 
           <Box mt={6}>
             <Typography variant="h4" gutterBottom>
-              Est il possible possible d'allier performance, économie de carburant et prix d'achat faible ?
+              Est-il possible d'allier performance, économie de carburant et prix d'achat faible ?
             </Typography>
             <Graph3 /> 
           </Box>
           
           <Box mt={6}>
             <Typography variant="h4" gutterBottom>
-              La diversité des gammes constructeurs ont il un impact sur leurs popularités ?
+              La diversité des gammes constructeurs a-t-elle un impact sur leurs popularités ?
             </Typography>
             <Graph4 />
           </Box>
